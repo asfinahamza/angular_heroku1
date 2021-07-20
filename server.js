914591ea50338f975579d12f5f96fdb0033248a8
asfinahamza <asfinahamza@gmail.com>
2021-07-20 10:30:31 +0530

    20/july/2021-2nd

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myapp'));
+app.use(express.static('./dist/my-app'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/myapp/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
